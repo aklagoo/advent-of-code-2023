c563759bd3b5bdd535817d612456d2d39ad5bea8
Archan Lagoo <aklagoo@gmail.com>
2023-12-04 12:36:03 -0700

    Completed solution 1

--- 1/main.ts
+++ 1/main.ts
@@ -2,6 +2,32 @@ export function parse(contents: string): string[] {
   return contents.split(/\r?\n/);
 }
 
+function parseWord(s: string): number {
+  let a: string = '', b: string = '';
+  let i = 0;
+
+  // Get first character
+  while (i < s.length) {
+    i++;
+    if (s[i-1] >= '0' && s[i-1] <= '9')
+      break;
+  }
+  a = s[i-1];
+
+  // Get last character
+  while (i < s.length) {
+    i++;
+    if (s[i-1] >= '0' && s[i-1] <= '9')
+      b = s[i-1];
+  }
+
+  if (b == '') b = a;
+  return parseInt(a+b);
+}
+
 export function main(data: string[]) {
-    console.log(data);
+  const vals = data.map(x => parseWord(x));
+  console.log(vals);
+  const sum = vals.reduce((acc, val) => acc + val, 0);
+  console.log(sum);
 }
